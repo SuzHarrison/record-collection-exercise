a86f4eb25d069514db2c03eb99d790a2c54fc4cb
Suzanne Harrison <harrison_family@hotmail.com>
2016-04-11 16:19:55 -0700

    lib setup_db. added gems.

---
+++ lib/setup_db.rb
@@ -0,0 +1,36 @@
+require 'sqlite3'
+
+class ReleaseDatabase
+  attr_reader :db
+
+  def initialize(dbname = "releases")
+    @db = SQLite3::Database.new "database/#{dbname}.db"
+  end
+
+  def reset_schema!
+    query = <<-CREATESTATEMENT
+      CREATE TABLE albums (
+      id INTEGER PRIMARY KEY,
+      label_code BLOB,
+      artist TEXT NOT NULL,
+      title TEXT NOT NULL,
+      label TEXT,
+      format TEXT,
+      released INTEGER,
+      date_added TEXT
+      );
+    CREATESTATEMENT
+
+    @db.execute("DROP TABLE IF EXISTS albums;")
+    @db.execute(query)
+    # How do we query a database from Ruby?
+    # table name: albums
+    # label_code,artist,title,label,format,released,date_added
+    # BLOB,       TEXT, TEXT, TEXT, TEXT,  INTEGER, TEXT
+    # one table
+  end
+end
+
+release_db = ReleaseDatabase.new
+# potato = ReleaseDatabase.new #same database but differenct instance
+release_db.reset_schema!
